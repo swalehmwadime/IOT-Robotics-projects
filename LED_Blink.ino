56ea8a7762f188361b774ff72fe6b6873e78d2c4
unknown <KONZA-VDI@GROUP17-011.vdi.konza>
2024-02-01 11:33:37 +0300

    LED_Blink.ino file

---
+++ LED_Blink.ino
@@ -0,0 +1,15 @@
+#define LED 12
+
+void setup() {
+//put your setup code here, to run once:
+pinMode(LED, OUTPUT);
+
+}
+void loop() {
+//put your main code here, to run repeat:
+digitalWrite(LED, HIGH );
+delay(100);
+digitalWrite(LED, LOW);
+delay(500);
+
+}
